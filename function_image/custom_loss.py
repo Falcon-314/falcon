45e4941cf7632d3dcec0eac313f7b9ab77138336
Falcon <65850235+Falcon-314@users.noreply.github.com>
2021-02-21 22:31:40 +0900

    Update and rename preprocess.py to custom_loss.py

---
+++ function_image/custom_loss.py
@@ -0,0 +1,86 @@
+import os
+import math
+import time
+import random
+import shutil
+from pathlib import Path
+from contextlib import contextmanager
+from collections import defaultdict, Counter
+
+import scipy as sp
+import numpy as np
+import pandas as pd
+
+from sklearn import preprocessing
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import StratifiedKFold
+
+from tqdm.auto import tqdm
+from functools import partial
+
+import cv2
+from PIL import Image
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.models as models
+from torch.nn.parameter import Parameter
+from torch.utils.data import DataLoader, Dataset
+
+#loss
+class LabelSmoothingLoss(nn.Module): 
+    def __init__(self, classes=CFG.target_size, smoothing=0.0, dim=-1): 
+        super(LabelSmoothingLoss, self).__init__() 
+        self.confidence = 1.0 - smoothing 
+        self.smoothing = smoothing 
+        self.cls = classes 
+        self.dim = dim 
+    def forward(self, pred, target): 
+        pred = pred.log_softmax(dim=self.dim) 
+        with torch.no_grad():
+            true_dist = torch.zeros_like(pred) 
+            true_dist.fill_(self.smoothing / (self.cls - 1)) 
+            true_dist.scatter_(1, target.data.unsqueeze(1), self.confidence) 
+        return torch.mean(torch.sum(-true_dist * pred, dim=self.dim))
+
+class TaylorSoftmax(nn.Module):
+    '''
+    This is the autograd version
+    '''
+    def __init__(self, dim=1, n=2):
+        super(TaylorSoftmax, self).__init__()
+        assert n % 2 == 0
+        self.dim = dim
+        self.n = n
+
+    def forward(self, x):
+        '''
+        usage similar to nn.Softmax:
+            >>> mod = TaylorSoftmax(dim=1, n=4)
+            >>> inten = torch.randn(1, 32, 64, 64)
+            >>> out = mod(inten)
+        '''
+        fn = torch.ones_like(x)
+        denor = 1.
+        for i in range(1, self.n+1):
+            denor *= i
+            fn = fn + x.pow(i) / denor
+        out = fn / fn.sum(dim=self.dim, keepdims=True)
+        return out
+
+class TaylorCrossEntropyLoss(nn.Module):
+    def __init__(self, n=2, ignore_index=-1, reduction='mean', smoothing=0.05):
+        super(TaylorCrossEntropyLoss, self).__init__()
+        assert n % 2 == 0
+        self.taylor_softmax = TaylorSoftmax(dim=1, n=n)
+        self.reduction = reduction
+        self.ignore_index = ignore_index
+        self.lab_smooth = LabelSmoothingLoss(CFG.target_size, smoothing=smoothing)
+
+    def forward(self, logits, labels):
+        log_probs = self.taylor_softmax(logits).log()
+        #loss = F.nll_loss(log_probs, labels, reduction=self.reduction,
+        #        ignore_index=self.ignore_index)
+        loss = self.lab_smooth(log_probs, labels)
+        return loss
