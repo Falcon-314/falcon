d893739c65bd6c7346bf25221a4b4795dd21e2f1
Falcon <65850235+Falcon-314@users.noreply.github.com>
2021-02-21 23:36:33 +0900

    Update statistical_value.py

--- function/statistical_value.py
+++ function/statistical_value.py
@@ -6,21 +6,9 @@ def range_diff(x):
 def range_ratio(x):
     return x.max() / x.min()
 
-#平均に対する割合を計算
-def ratio(x):
-    return x / x.mean()
-
-#平均に対する差を計算
-def mean_diff(x):
-    return x - x.mean()
-
 #平均と偏差の比
 def mean_variance(x):
     return x.std() / x.mean()
-
-#Z score
-def z_score(x):
-    return ( x - x.mean() ) / x.std()
 
 # 第一四分位点
 def third_quartile(x):
@@ -35,8 +23,6 @@ def quartile_range(x):
     return x.quantile(0.75) - x.quantile(0.25)
 
 #平均値より大きい値の数と小さい値の数の比
-def hl_ratio(x):
-    um(ifelse(x > mean(x),1,0)) / sum(ifelse(x >= mean(x),0,1))
     
 #MAD : Median Absolute Deviation : median( |x - median(x)| )
 
