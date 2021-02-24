306d68599458beccc9c701d0a8dd8067b1212b8e
Falcon <65850235+Falcon-314@users.noreply.github.com>
2021-02-24 22:09:28 +0900

    Update gbdt.py

--- model/gbdt.py
+++ model/gbdt.py
@@ -1,1 +1,150 @@
+import lightgbm as lgb
+import xgboost as xgb
+from catboost import CatBoost
+from catboost import Pool
+from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import Ridge
 
+from abc import abstractmethod
+class Base_Model(object):
+    @abstractmethod
+    def fit(self, x_train, y_train, x_valid, y_valid):
+        raise NotImplementedError
+
+    @abstractmethod
+    def predict(self, model, features):
+        raise NotImplementedError
+
+    def cv(self, y_train, train_features, test_features, fold_ids):
+        test_preds = np.zeros(len(test_features))
+        oof_preds = np.zeros(len(train_features))
+
+        for i_fold, (trn_idx, val_idx) in enumerate(fold_ids):
+            x_trn = train_features.iloc[trn_idx]
+            y_trn = y_train[trn_idx]
+            x_val = train_features.iloc[val_idx]
+            y_val = y_train[val_idx]
+
+            model = self.fit(x_trn, y_trn, x_val, y_val)
+
+            oof_preds[val_idx] = self.predict(model, x_val)
+            oof_score = np.sqrt(mean_squared_error(y_val, oof_preds[val_idx]))
+            print('fold{}:RMSLE{}'.format(i_fold,oof_score))
+            test_preds += self.predict(model, test_features) / len(fold_ids)
+
+        oof_score = np.sqrt(mean_squared_error(y_train, oof_preds))
+        print(f'oof score: {oof_score}')
+
+        evals_results = {"evals_result": {
+            "oof_score": oof_score,
+            "n_data": len(train_features),
+            "n_features": len(train_features.columns),
+        }}
+
+        return oof_preds, test_preds, evals_results
+cat_col = []
+class Lgbm(Base_Model):
+    def __init__(self,model_params):
+        self.model_params = model_params
+        self.models = []
+        self.feature_cols = None
+
+    def fit(self,x_train,y_train,x_valid,y_valid):
+        lgb_train = lgb.Dataset(x_train,y_train)
+        lgb_valid = lgb.Dataset(x_valid,y_valid)
+
+        model = lgb.train(self.model_params,
+            train_set=lgb_train,
+            valid_sets=[lgb_valid],
+            valid_names=['valid'],
+            categorical_feature=cat_col,
+            early_stopping_rounds=20,
+            num_boost_round=10000,
+            verbose_eval=False)
+        self.models.append(model)
+        return model
+
+    def predict(self,model,features):
+        self.feature_cols = features.columns
+        return model.predict(features)
+
+    def visualize_importance(self):
+        feature_importance_df = pd.DataFrame()
+
+        for i,model in enumerate(self.models):
+            _df = pd.DataFrame()
+            _df['feature_importance'] = model.feature_importance(importance_type='gain')
+            _df['column'] = self.feature_cols
+            _df['fold'] = i+1
+            feature_importance_df = pd.concat([feature_importance_df,_df],axis=0,ignore_index=True)
+
+        order = feature_importance_df.groupby('column').sum()[['feature_importance']].sort_values('feature_importance',ascending=False).index[:50]
+
+        fig, ax = plt.subplots(figsize=(max(6, len(order) * .4), 7))
+        sns.boxenplot(data=feature_importance_df, x='column', y='feature_importance', order=order, ax=ax, palette='viridis')
+        ax.tick_params(axis='x', rotation=90)
+        ax.grid()
+        fig.tight_layout()
+        return fig,ax
+
+class Cat(Base_Model):
+    def __init__(self,model_params):
+        self.model_params = model_params
+    def fit(self,x_train,y_train,x_valid,y_valid):
+        train_pool = Pool(x_train,
+                          label=y_train,
+                          cat_features=cat_col)
+        valid_pool = Pool(x_valid,
+                          label=y_valid,
+                          cat_features=cat_col)
+
+        model = CatBoost(self.model_params)
+        model.fit(train_pool,
+                  early_stopping_rounds=30,
+                 plot=False,
+                 use_best_model=True,
+                 eval_set=[valid_pool],
+                  verbose=False)
+
+        return model
+
+    def predict(self,model,features):
+      pred = model.predict(features)
+      return pred
+
+class Xgb(Base_Model):
+    def __init__(self,model_params):
+        self.model_params = model_params
+
+    def fit(self,x_train,y_train,x_valid,y_valid):
+        xgb_train = xgb.DMatrix(x_train,label=y_train)
+        xgb_valid = xgb.DMatrix(x_valid,label=y_valid)
+
+        evals = [(xgb_train,'train'),(xgb_valid,'eval')]
+
+        model = xgb.train(self.model_params,
+                         xgb_train,
+                         num_boost_round=2000,
+                         early_stopping_rounds=20,
+                         evals=evals,
+                         verbose_eval=False)
+
+        return model
+
+    def predict(self,model,features):
+        return model.predict(xgb.DMatrix(features))
+
+class Rid(Base_Model):
+    def __init__(self):
+      self.model = None
+    def fit(self,x_train,y_train,x_valid,y_valid):
+        model =Ridge(
+            alpha=1, #L2係数
+            max_iter=1000,
+            random_state=10,
+                              )
+        model.fit(x_train,y_train)
+        return model
+
+    def predict(self,model,features):
+      return model.predict(features)
