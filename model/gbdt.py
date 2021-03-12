f8ceb6c29f80697e66e7d8756eccb672f3848aa5
Falcon <65850235+Falcon-314@users.noreply.github.com>
2021-03-13 00:34:58 +0900

    Update gbdt.py

--- model/gbdt.py
+++ model/gbdt.py
@@ -59,30 +59,17 @@ class Lgbm(Base_Model):
         self.fit(x_train,y_train,x_valid,y_valid)
         oof_df = self.predict(x_valid)
         return oof_df, self.model       
-
-    def visualize(self, importance_df, size = (8,8)):
-       cols = (importance_df[["Feature", "importance"]]
-            .groupby("Feature")
-            .mean()
-            .sort_values(by="importance", ascending=False)[:50].index)
-
-       best_features = importance_df.loc[importance_df.Feature.isin(cols)]
-
-       plt.figure(figsize=size)
-       sns.barplot(x="importance", y="Feature", data=best_features.sort_values(by="importance", ascending=False))
-       plt.title('Features importance (averaged/folds)')
-       plt.tight_layout() 
-    
    
-
+   
 class Cat(Base_Model):
-    def __init__(self,model_params):
+    def __init__(self,model_params, features_index):
         self.model_params = model_params
         self.model = None
+        self.features_index = features_index
 
     def fit(self,x_train,y_train,x_valid,y_valid):
-        train_pool = Pool(x_train,label=y_train,cat_index)
-        valid_pool = Pool(x_valid,label=y_valid,cat_index)
+        train_pool = Pool(x_train,label=y_train, cat_features = self.features_index)
+        valid_pool = Pool(x_valid,label=y_valid, cat_features = self.features_index)
         
         model = CatBoost(self.model_params)
         model.fit(train_pool,
@@ -114,22 +101,41 @@ class Xgb(Base_Model):
         evals = [(xgb_train,'train'),(xgb_valid,'eval')]
 
         model = xgb.train(self.model_params,
-                         xgb_train,
-                         num_boost_round=3000,
-                         early_stopping_rounds=50,
-                         evals=evals,
-                         verbose_eval=False,
-                         callbacks=[wandb.xgboost.wandb_callback()]) #custom metricを使用する場合obj = ***を追加
-        
+                          xgb_train,
+                          num_boost_round=2000,
+                          early_stopping_rounds=20,
+                          evals=evals,
+                          verbose_eval=False,
+                          callbacks=[wandb.xgboost.wandb_callback()]) #obj=huber_approx_obj...
+       
         self.model = model
     
     def predict(self,x_test):
-        return self.model.predict(xgb.DMatrix(x_test))
+        dtest = xgb.DMatrix(x_test)
+        return self.model.predict(dtest)
         
     def train(self,x_train,y_train,x_valid,y_valid):
         self.fit(x_train,y_train,x_valid,y_valid)
         oof_df = self.predict(x_valid)
         return oof_df, self.model
+    
+    def importance(self, features, fold):
+        fold_importance_df = pd.DataFrame()
+        fold_importance_df["Feature"] = features
+        fold_importance_df["importance"] = self.model.feature_importances_
+        fold_importance_df["fold"] = fold
+        return fold_importance_df
 
 
+  def visualize(importance_df, size = (8,8)):
+       cols = (importance_df[["Feature", "importance"]]
+            .groupby("Feature")
+            .mean()
+            .sort_values(by="importance", ascending=False)[:50].index)
 
+       best_features = importance_df.loc[importance_df.Feature.isin(cols)]
+
+       plt.figure(figsize=size)
+       sns.barplot(x="importance", y="Feature", data=best_features.sort_values(by="importance", ascending=False))
+       plt.title('Features importance (averaged/folds)')
+       plt.tight_layout() 
