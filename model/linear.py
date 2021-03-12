a09f00380e92900b7ecc24c7d7eddd36e6a99f36
Falcon <65850235+Falcon-314@users.noreply.github.com>
2021-03-13 01:51:46 +0900

    Update linear.py

--- model/linear.py
+++ model/linear.py
@@ -1,14 +1,87 @@
+from sklearn.linear_model import Ridge, Lasso, LinearRegression, BayesianRidge
+
+from abc import abstractmethod
+class Base_Model(object):
+    @abstractmethod
+    def fit(self, x_train, y_train, x_valid, y_valid):
+        raise NotImplementedError
+
+    @abstractmethod
+    def predict(self, model, features):
+        raise NotImplementedError
+
 class Rid(Base_Model):
-    def __init__(self):
-      self.model = None
-    def fit(self,x_train,y_train,x_valid,y_valid):
-        model =Ridge(
-            alpha=1, #L2係数
-            max_iter=1000,
-            random_state=10,
-                              )
+    def __init__(self, model_params):
+        self.model_params = model_params
+        self.model = None
+    
+    def fit(self,x_train,y_train):
+        model =Ridge(self.model_params)
         model.fit(x_train,y_train)
+        self.model = model
         return model
 
-    def predict(self,model,features):
-      return model.predict(features)
+    def predict(self,x_test):
+        return self.model.predict(x_test)
+
+    def train(self,x_train,y_train,x_valid,y_valid):
+        self.fit(x_train,y_train)
+        oof_df = self.predict(x_valid)
+        return oof_df, self.model   
+
+class Lasso(Base_Model):
+    def __init__(self, model_params):
+        self.model_params = model_params
+        self.model = None
+    
+    def fit(self,x_train,y_train):
+        model =Lasso(self.model_params)
+        model.fit(x_train,y_train)
+        self.model = model
+        return model
+
+    def predict(self,x_test):
+        return self.model.predict(x_test)
+
+    def train(self,x_train,y_train,x_valid,y_valid):
+        self.fit(x_train,y_train)
+        oof_df = self.predict(x_valid)
+        return oof_df, self.model   
+    
+ class LinearReg(Base_Model):
+    def __init__(self, model_params):
+        self.model_params = model_params
+        self.model = None
+    
+    def fit(self,x_train,y_train):
+        model =LinearRegression()
+        model.fit(x_train,y_train)
+        self.model = model
+        return model
+
+    def predict(self,x_test):
+        return self.model.predict(x_test)
+
+    def train(self,x_train,y_train,x_valid,y_valid):
+        self.fit(x_train,y_train)
+        oof_df = self.predict(x_valid)
+        return oof_df, self.model      
+    
+class BayesReg(Base_Model):
+    def __init__(self, model_params):
+        self.model_params = model_params
+        self.model = None
+    
+    def fit(self,x_train,y_train):
+        model =BayesianRidge(compute_score=True)
+        model.fit(x_train,y_train)
+        self.model = model
+        return model
+
+    def predict(self,x_test):
+        return self.model.predict(x_test)
+
+    def train(self,x_train,y_train,x_valid,y_valid):
+        self.fit(x_train,y_train)
+        oof_df = self.predict(x_valid)
+        return oof_df, self.model      
