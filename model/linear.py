587365656980f4144fdc0963bde5d8daafda3fb5
Falcon <65850235+Falcon-314@users.noreply.github.com>
2021-03-13 02:07:48 +0900

    Update linear.py

--- model/linear.py
+++ model/linear.py
@@ -1,4 +1,4 @@
-from sklearn.linear_model import Ridge, Lasso, LinearRegression, BayesianRidge
+from sklearn.linear_model import Ridge, Lasso, LinearRegression, BayesianRidge, LogisticRegression
 
 from abc import abstractmethod
 class Base_Model(object):
@@ -85,3 +85,22 @@ class BayesReg(Base_Model):
         self.fit(x_train,y_train)
         oof_df = self.predict(x_valid)
         return oof_df, self.model      
+
+class LogReg(Base_Model):
+    def __init__(self, model_params):
+        self.model_params = model_params
+        self.model = None
+    
+    def fit(self,x_train,y_train):
+        model =LogisticRegression(self.model_params)
+        model.fit(x_train,y_train)
+        self.model = model
+        return model
+
+    def predict(self,x_test):
+        return self.model.predict(x_test)
+
+    def train(self,x_train,y_train,x_valid,y_valid):
+        self.fit(x_train,y_train)
+        oof_df = self.predict(x_valid)
+        return oof_df, self.model   
