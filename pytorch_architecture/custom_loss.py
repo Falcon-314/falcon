ae83264a7daba1b12b727a7de9eb3e194257faa4
Falcon <65850235+Falcon-314@users.noreply.github.com>
2021-04-05 22:43:18 +0900

    Update custom_loss.py

--- pytorch_architecture/custom_loss.py
+++ pytorch_architecture/custom_loss.py
@@ -1,1 +1,13 @@
+def get_criterion(CFG):
+    if CFG.criterion == 'MSELoss':
+        criterion =  nn.MSELoss()
+    elif CFG.criterion == 'L1Loss':
+        criterion =  nn.L1Loss()
+    elif CFG.criterion == 'BCELoss':
+        criterion =  nn.BCELoss()
+    elif CFG.criterion == 'BCEWithLogitsLoss':
+        criterion =  nn.BCEWithLogitsLoss()
+    elif CFG.criterion == 'CrossEntropyLoss':
+        criterion =  nn.CrossEntropyLoss()
 
+    return criterion
