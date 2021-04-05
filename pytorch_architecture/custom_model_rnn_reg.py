db8e3e0db6a0b6d992f6bda3d4eb0564cfdca621
Falcon <65850235+Falcon-314@users.noreply.github.com>
2021-04-05 22:46:35 +0900

    Update and rename custom_model.py to custom_model_rnn_reg.py

--- pytorch_architecture/custom_model_rnn_reg.py
+++ pytorch_architecture/custom_model_rnn_reg.py
@@ -1,21 +1,3 @@
-class MLP_2layer(nn.Module):
-    def __init__(self, CFG, x_train, targets):
-        super().__init__()
-        self.mlp = nn.Sequential(nn.Linear(CFG.hidden_size*2, CFG.hidden_size),
-                          nn.BatchNorm1d(CFG.hidden_size),
-                          nn.Dropout(CFG.dropout),
-                          nn.ReLU(),
-                          nn.Linear(CFG.hidden_size, CFG.hidden_size),
-                          nn.BatchNorm1d(CFG.hidden_size),
-                          nn.Dropout(CFG.dropout),
-                          nn.ReLU(),
-                          nn.Linear(CFG.hidden_size, 22))
-
-    def forward(self, x):
-        x = self.mlp(x)
-        return x
-
-
 class LSTM_2layer(nn.Module):
     def __init__(self, CFG, x_train, targets):
         super().__init__()
