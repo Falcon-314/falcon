76df94a20a9cdebadc48d7810bf86fd2fb8f21f8
Falcon <65850235+Falcon-314@users.noreply.github.com>
2021-04-14 21:07:52 +0900

    Update create_fold.py

--- function/create_fold.py
+++ function/create_fold.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import GroupKFold
 def cvsplit(CFG, train):
     if CFG.fold_type == 'KFold':
         Fold = KFold(n_splits=CFG.n_fold, shuffle=True, random_state=CFG.seed)
-        for n, (train_index, val_index) in enumerate(Fold.split(train,train[CFG.target_col])):
+        for n, (train_index, val_index) in enumerate(Fold.split(train,train[CFG.target_cols])):
             train.loc[val_index, 'fold'] = int(n)
         train['fold'] = train['fold'].astype(int)
         
@@ -18,7 +18,7 @@ def cvsplit(CFG, train):
 
     elif CFG.fold_type == 'GroupKFold':
         Fold = GroupKFold(n_splits=CFG.n_fold)
-        for n, (train_index, val_index) in enumerate(Fold.split(train,train[CFG.target_col],train[CFG.group_col])):
+        for n, (train_index, val_index) in enumerate(Fold.split(train,train[CFG.target_cols],train[CFG.group_col])):
             train.loc[val_index, 'fold'] = int(n)
         train['fold'] = train['fold'].astype(int)
     
