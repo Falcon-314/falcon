51bdb66b1dd457a249c62c9fbf2a182649f53507
Falcon <65850235+Falcon-314@users.noreply.github.com>
2021-04-21 23:21:34 +0900

    Update FE_basic.py

--- feature_engineering/FE_basic.py
+++ feature_engineering/FE_basic.py
@@ -54,29 +54,30 @@ class CalcBlock(BaseBlock):
         self.mode = mode
         
     def fit(self,df):
+        df_tmp = pd.DataFrame(df[self.CFG.ID_col])
         if self.mode == 'plus':
-            df[self.col1 + '_plus_' + self.col2] = df[self.col1] + df[self.col2]
-            self.meta_df = df[[self.CFG.ID_col, self.col1 + '_plus_' + self.col2]]
+            df_tmp[self.col1 + '_plus_' + self.col2] = df[self.col1] + df[self.col2]
+            self.meta_df = df_tmp[[self.CFG.ID_col, self.col1 + '_plus_' + self.col2]]
         if self.mode == 'minus':
-            df[self.col1 + '_minus_' + self.col2] = df[self.col1] - df[self.col2]
-            self.meta_df = df[[self.CFG.ID_col, self.col1 + '_minus_' + self.col2]]
+            df_tmp[self.col1 + '_minus_' + self.col2] = df[self.col1] - df[self.col2]
+            self.meta_df = df_tmp[[self.CFG.ID_col, self.col1 + '_minus_' + self.col2]]
         if self.mode == 'times':
-            df[self.col1 + '_times_' + self.col2] = df[self.col1] - df[self.col2]
-            self.meta_df = df[[self.CFG.ID_col, self.col1 + '_times_' + self.col2]]
+            df_tmp[self.col1 + '_times_' + self.col2] = df[self.col1] * df[self.col2]
+            self.meta_df = df_tmp[[self.CFG.ID_col, self.col1 + '_times_' + self.col2]]
         if self.mode == 'devided':
-            df[self.col1 + '_devided_' + self.col2] = df[self.col1] - df[self.col2]
-            self.meta_df = df[[self.CFG.ID_col, self.col1 + '_devided_' + self.col2]]
+            df_tmp[self.col1 + '_devided_' + self.col2] = df[self.col1] / df[self.col2]
+            self.meta_df = df_tmp[[self.CFG.ID_col, self.col1 + '_devided_' + self.col2]]
         return self
 
     def transform(self,df):
         if self.mode == 'plus':
-            self.return_df = pd.merge(df, self.meta_df, on = self.CFG.ID_col, how = 'left')[[self.CFG.ID_col,self.col1 + '_plus_' + self.col2]]
+            self.return_df = pd.merge(df, self.meta_df, on = self.CFG.ID_col, how = 'left')[[self.CFG.ID_col, self.col1 + '_plus_' + self.col2]]
         if self.mode == 'minus':
-            self.return_df = pd.merge(df, self.meta_df, on = self.CFG.ID_col, how = 'left')[[self.CFG.ID_col,self.col1 + '_minus_' + self.col2]]
+            self.return_df = pd.merge(df, self.meta_df, on = self.CFG.ID_col, how = 'left')[[self.CFG.ID_col, self.col1 + '_minus_' + self.col2]]
         if self.mode == 'times':
-            self.return_df = pd.merge(df, self.meta_df, on = self.CFG.ID_col, how = 'left')[[self.CFG.ID_col,self.col1 + '_times_' + self.col2]]
+            self.return_df = pd.merge(df, self.meta_df, on = self.CFG.ID_col, how = 'left')[[self.CFG.ID_col, self.col1 + '_times_' + self.col2]]
         if self.mode == 'devided':
-            self.return_df = pd.merge(df, self.meta_df, on = self.CFG.ID_col, how = 'left')[[self.CFG.ID_col,self.col1 + '_devided_' + self.col2]]
+            self.return_df = pd.merge(df, self.meta_df, on = self.CFG.ID_col, how = 'left')[[self.CFG.ID_col, self.col1 + '_devided_' + self.col2]]
         return self
       
 # =======================
