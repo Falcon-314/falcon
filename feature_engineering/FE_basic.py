b882b7f40d6beb443948b658a12b1bb4fc8b9ea8
Falcon <65850235+Falcon-314@users.noreply.github.com>
2021-04-22 23:52:11 +0900

    Update FE_basic.py

--- feature_engineering/FE_basic.py
+++ feature_engineering/FE_basic.py
@@ -29,14 +29,6 @@ class BaseBlock(object):
 
     def save(self, filename):
         self.return_df.to_csv(self.CFG.FEATURE_PATH + filename + '.csv',index=False)
-      
-# =======================
-# Feature Loading
-# =======================
-def feature_load(CFG, df, file_name):
-    load_df = pd.read_csv(CFG.FEATURE_PATH + file_name)
-    df = pd.merge(df,load_df,on = CFG.ID_col, how = 'left')
-    return df
 
 # =======================
 # Basic Feature Engineering
