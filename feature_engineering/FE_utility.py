53ccc60d33987438267a9b296e0c36391a14c566
Falcon <65850235+Falcon-314@users.noreply.github.com>
2021-04-26 23:12:24 +0900

    Update FE_utility.py

--- feature_engineering/FE_utility.py
+++ feature_engineering/FE_utility.py
@@ -1,26 +1,5 @@
 import pandas as pd
 import numpy as np
-
-#===========================
-#BaseBlockの設定
-#===========================
-class BaseBlock(object):
-    def __init__(self, CFG):
-        self.CFG = CFG
-
-    def fit(self,input_df):
-        self.meta_df = input_df
-        return self
-    
-    def transform(self,input_df):
-        self.return_df = input_df
-        return self
-    
-    def view(self):
-        return self.return_df
-
-    def save(self, filename):
-        self.return_df.to_csv(self.CFG.FEATURE_PATH + filename + '.csv',index=False)
         
 # =======================
 # Feature Loading
