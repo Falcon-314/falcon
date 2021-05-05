c38da49170e09c89da88504a8572397b1e417a3d
Falcon <65850235+Falcon-314@users.noreply.github.com>
2021-05-05 19:30:54 +0900

    Update model_base.py

--- model/model_base.py
+++ model/model_base.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import pickle
 
 def table_train(CFG, train, custommodel, dataset_preprocess_train, dataset_postprocess_train, get_result, MODEL_NAME, LOGGER):
     oof_df = pd.DataFrame()
